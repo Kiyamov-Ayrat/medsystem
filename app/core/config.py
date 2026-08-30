91bf8768e0f9526660b56c7af91f808cc0e8500b
Ayrat <kiamovajrat1@gmail.com>
2026-08-31 02:14:56 +0500

    create docker compose, create db in config

--- app/core/config.py
+++ app/core/config.py
@@ -14,3 +14,18 @@ class Settings(BaseSettings):
     secret_key: str
     access_token_expire_minutes: int = 30
     refresh_token_expire_days: int = 30
+
+    # PostgreSQL
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str
+    postgres_host: str
+    postgres_port: int
+    #
+    # @property
+    # def database_url(self) -> str:
+    #
+    #     return (
+    #         f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}"
+    #         f"@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
+    #     )
